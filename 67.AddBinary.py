3d5113eae0e92c3bd8bb591f7f797a7d3b2a2b54
Darshan <darshanp21198@gmail.com>
2025-01-24 02:20:40 -0500

    24th Jan 25

---
+++ 67.AddBinary.py
@@ -0,0 +1,15 @@
+"""
+67. Add Binary
+"""
+def addBinary(self, a, b):
+   
+        a, b = int(a, 2), int(b, 2)
+
+        while b:
+            without_carry = a^b
+            carry = (a & b) << 1
+            a,b = without_carry, carry
+        
+        return bin(a)[2:]
+        #Time: O(A+B)
+        #Space: O(1)
