c4d237593fbdee15077e06af9412351417ebafa1
Darshan <darshanp21198@gmail.com>
2025-02-28 02:52:55 -0500

    022825 Leetcode 138

---
+++ 138.CopyListwithRandomPointer.py
@@ -0,0 +1,24 @@
+"""
+138. Copy List with Random Pointer 
+
+Time Complexity: O(N)
+Space Complexity: O(N)
+
+"""
+def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+    oldToCopy = {None: None}
+
+    cur = head 
+    while cur:
+        copy = Node(cur.val)
+        oldToCopy[cur] = copy
+        cur = cur.next
+    
+    cur = head 
+    while cur:
+        copy = oldToCopy[cur]
+        copy.next = oldToCopy[cur.next]
+        copy.random = oldToCopy[cur.random]
+        cur = cur.next
+    
+    return oldToCopy[head]
